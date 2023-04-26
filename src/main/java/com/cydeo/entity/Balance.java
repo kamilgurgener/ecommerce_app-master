782524eec32e72be0f5df197e6f6bcfe7ce01a84
kamilgurgener <kamilgurgener@gmail.com>
2023-04-26 19:35:54 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/entity/Balance.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Balance extends BaseEntity {
+
+    private BigDecimal amount;
+
+    @OneToOne
+    private Customer customer;
+
+}
---
+++ src/main/java/com/cydeo/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+
+
+}
